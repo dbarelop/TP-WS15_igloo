310c0093ddcba584691faa87a4cef6b4fbe0338d
Matthias Weis <matweis@htwg-konstanz.de>
2015-10-26 16:55:32 +0100

    progress, EWEN/DS missing

--- EEPROM93LC66/testbench/EEPROM93LC66.vhd
+++ EEPROM93LC66/testbench/EEPROM93LC66.vhd
@@ -13,8 +13,8 @@ END EEPROM93LC66;
 
 ARCHITECTURE simulation OF EEPROM93LC66 IS
   type      memory_array  is array(0 to 4095) of std_logic_vector(7 downto 0);
-  type			tstate IS (IDLE, RXSB, RXOP, RXOP2, RXADDR, RXDIN, TXDOUT, MEMBUSY);
-  type			tcmd IS (NONE, ERASE, RE4D, WR1TE, WRAL);
+  type			tstate IS (IDLE, RXSB, RXOP, RXOP2, RXADDR, WAITFORCS, RXDIN, TXDOUT, MEMBUSY);
+  type			tcmd IS (NONE, ERASE, ERAL, RE4D, WR1TE, WRAL);
 	
 	signal MEM_DATA			: memory_array <=(others '1');
 
@@ -33,10 +33,54 @@ BEGIN
 		IF rising_edge(cs) THEN
 			IF state = IDLE THEN
 				state = RXSB;
+			ELSIF state = MEMBUSY THEN
+				do <= '0';
 			END IF;
-
+		ELSIF falling_edge(cs) AND state = WAITFORCS THEN
+			IF cmd = ERASE THEN
+				IF org = '1' THEN
+					memory_array(address * 2) <= (others '1');
+					memory_array((address * 2) + 1) <= (others '1');
+				ELSE
+					memory_array(address) <= (others '1');
+				END IF;
+				cmd <= NONE;
+				state <= MEMBUSY;
+			ELSIF cmd = ERAL THEN
+				memory_array <= (others '1');
+				cmd <= NONE;
+				state <= MEMBUSY;
+			ELSIF cmd = WR1TE THEN
+				IF org = '1' THEN
+					memory_array(address*2) <= serialInR(15 DOWNTO 8);
+					memory_array((address*2)+1) <= serialInR(7 DOWNTO 0);
+				ELSE
+					memory_array(address) <= serialInR(7 DOWNTO 0);
+				END IF;
+				cmd <= NONE;
+				state <= MEMBUSY;
+			ELSIF cmd = WRAL THEN
+				IF org = '1' THEN
+					for i in 0 to 2047 LOOP
+						memory_array(i*2) <= serialInR(15 DOWNTO 8);
+						memory_array((i*2)+1) <= serialInR(7 DOWNTO 0);
+					END LOOP;
+				ELSE
+					memory_array <= serialInR(7 DOWNTO 0);
+				END IF;
+				cmd <= NONE;
+				state <= MEMBUSY;
+			END IF;
 		END IF;
 
+	END PROCESS;
+
+	busyState: PROCESS(state) IS
+
+	BEGIN
+		IF state = MEMBUSY THEN
+			state <= IDLE AFTER 
+		END IF;
 	END PROCESS;
 
 	serialInPro: PROCESS(sclk) IS
@@ -80,8 +124,8 @@ BEGIN
 						CASE serialInR(8 DOWNTO 7) IS
 							WHEN "10" =>
 								-- ERAL
-								memory_array <= (others '1');
-								state <= MEMBUSY;
+								cmd <= ERAL;
+								state <= WAITFORCS;
 							WHEN "00" =>
 								-- EWDS
 								writeProtect <= '1';
@@ -101,18 +145,11 @@ BEGIN
 				WHEN RXADDR =>
 					serialInR <= serialInR(14 DOWNTO 0) & di;
 					cnt := cnt + 1;
-					IF (org = '1' AND cnt = 8) OR (org = '0' and cnt = 9) THEN
+					IF (org = '1' AND cnt = 8) OR (org = '0' AND cnt = 9) THEN
 						address <= serialInR(8 DOWNTO 0);
 						IF cmd = ERASE THEN
-							IF cnt = 8 THEN
-								memory_array(address * 2) <= (others '1');
-								memory_array((address * 2) + 1) <= (others '1');
-							ELSE
-								memory_array(address) <= (others '1');
-							END IF;
-							cmd <= NONE;
-							state <= MEMBUSY;
-							cnt := 0;
+							-- wait for falling edge in CS
+							state <= WAITFORCS;							
 						ELSIF cmd = RE4D THEN
 							-- DO = 0 at A0 missing!!
 							IF cnt = 8 THEN
@@ -122,39 +159,21 @@ BEGIN
 								serialOutR(15 DOWNTO 8) <= memory_array(address);
 							END IF;
 							state <= TXDOUT;
-							cnt = 0;
 						ELSIF cmd = WR1TE THEN
 							state <= RXDIN
-							cnt = 0;
 						END IF;
+						cnt := 0;
 					END IF;
 				WHEN RXDIN =>
 					serialInR <= serialInR(14 DOWNTO 0) & di;
 					cnt := cnt + 1;
-					IF (org = '1' AND cnt = 16) OR (org = '0' and cnt = 8) THEN
+					IF (org = '1' AND cnt = 16) OR (org = '0' AND cnt = 8) THEN
 						IF cmd = WR1TE THEN
-							IF cnt = 8 THEN
-								memory_array(address) <= serialInR(7 DOWNTO 0);
-							ELSE
-								memory_array(address*2) <= serialInR(15 DOWNTO 8);
-								memory_array((address*2)+1) <= serialInR(7 DOWNTO 0);
-							END IF;
-							cnt := 0;
-							cmd <= NONE;
-							state <= MEMBUSY;
+							state <= WAITFORCS;
 						ELSIF cmd = WRAL THEN
-							IF cnt = 8 THEN
-								memory_array <= serialInR(7 DOWNTO 0);
-							ELSE
-								for i in 0 to 2047 LOOP
-									memory_array(i*2) <= serialInR(15 DOWNTO 8);
-									memory_array((i*2)+1) <= serialInR(7 DOWNTO 0);
-								END LOOP;
-							END IF;
-							cnt := 0;
-							cmd <= NONE;
-							state <= MEMBUSY;
+							state <= WAITFORCS;
 						END IF;
+						cnt := 0;
 					END IF;
 			END CASE;
 		END IF;
@@ -169,7 +188,7 @@ BEGIN
 			do <= serialOutR(15);
 			serialOutR <= serialOutR(14 DOWNTO 0) & '0';
 			cnt := cnt + 1;
-			IF (org = '1' AND cnt = 16) OR (org = '0' and cnt = 8) THEN
+			IF (org = '1' AND cnt = 16) OR (org = '0' AND cnt = 8) THEN
 				cnt := 0;
 				state <= IDLE;
 			END IF;
