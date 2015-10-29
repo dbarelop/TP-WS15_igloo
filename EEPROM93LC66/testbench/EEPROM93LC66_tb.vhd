af797e2a7bd6d5f9163634bb221d2f47548d4d90
Andreas Reinhardt <andreasreinhardt7@gmail.com>
2015-10-29 12:02:50 +0100

    fixed names, added architecture

--- EEPROM93LC66/testbench/EEPROM93LC66_tb.vhd
+++ EEPROM93LC66/testbench/EEPROM93LC66_tb.vhd
@@ -1,4 +1,46 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
 
+ENTITY ADT7301_tb IS
+   -- empty
+END ADT7301_tb;
+
+ARCHITECTURE verhalten OF EEPROM93LC67_tb IS
+
+   CONSTANT RSTDEF: std_logic := '0';
+   CONSTANT FRQDEF: natural   := 1e6;
+   CONSTANT tcyc:   time      := 1 sec / FRQDEF;
+
+   COMPONENT EEPROM93LC67
+      PORT(sclk: IN  std_logic;  -- serial clock input
+           cs:   IN  std_logic;  -- chip select, low active
+           din:  IN  std_logic;  -- serial data input
+           dout: OUT std_logic; -- serial data output
+		   org:  IN std_logic); -- 8 or 16 bit
+   END COMPONENT;
+
+   SIGNAL rst:  std_logic := RSTDEF;
+   SIGNAL clk:  std_logic := '0';
+   SIGNAL cs:   std_logic := '1';
+   SIGNAL sclk: std_logic := '1';
+   SIGNAL miso: std_logic := '0';
+   SIGNAL mosi: std_logic := '0';
+   SIGNAL org: std_logic := '0';
+   
+   SIGNAL reg:  std_logic_vector(15 DOWNTO 0);
+
+BEGIN
+
+   rst <= RSTDEF, NOT RSTDEF AFTER 5 us;
+--   clk <= NOT clk AFTER tcyc/2;
+
+   u1: EEPROM93LC67
+   PORT MAP(sclk => sclk,
+            cs   => cs,
+            din  => mosi,
+            dout => miso,
+			org => org);
 
 
 PROCEDURE write (data: std_logic_vector; address: std_logic_vector)
