b168567d24bb835fcf861d6bf234148c7b0d1772
Matthias Weis <matweis@htwg-konstanz.de>
2015-10-31 08:47:55 +0100

    syntax fixes

--- EEPROM93LC66/testbench/EEPROM93LC66_tb.vhd
+++ EEPROM93LC66/testbench/EEPROM93LC66_tb.vhd
@@ -50,15 +50,15 @@ BEGIN
 		END PROCEDURE;
 		
 		PROCEDURE delete (address: std_logic_vector) IS
-			CONSTANT oppcode: std_logic_vector(1 DOWNTO 0) = "11";
+			CONSTANT oppcode: std_logic_vector(1 DOWNTO 0) := "11";
 		BEGIN
 			WAIT UNTIL clk'EVENT AND clk='1';
 			spi_write(oppcode & address);
 		END PROCEDURE;
 		
 		PROCEDURE delete_all IS
-			CONSTANT oppcode: std_logic_vector(1 DOWNTO 0) = "00";
-			CONSTANT address: std_logic_vector(1 DOWNTO 0) = "10";
+			CONSTANT oppcode: std_logic_vector(1 DOWNTO 0) := "00";
+			CONSTANT address: std_logic_vector(1 DOWNTO 0) := "10";
 		BEGIN
 			WAIT UNTIL clk'EVENT AND clk='1';
 			spi_write(oppcode & address);
@@ -86,7 +86,7 @@ BEGIN
 		PROCEDURE spi_read (VARIABLE data: out std_logic_vector(15 DOWNTO 0)) IS
 			--VARIABLE reg: std_logic_vector(15 DOWNTO 0);
 		BEGIN
-			FOR i IN data data
+			FOR i IN data LOOP
 				WAIT FOR tcyc;	
 				sclk <= '1';
 				WAIT FOR tcyc;
@@ -98,4 +98,4 @@ BEGIN
 	
 	END PROCESS;
 
-		
+END verhalten;
