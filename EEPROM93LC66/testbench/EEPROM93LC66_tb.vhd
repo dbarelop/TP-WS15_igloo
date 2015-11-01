d02f91ec55605489867ee51759e407e42518bb05
Matthias Weis <matweis@htwg-konstanz.de>
2015-11-01 16:56:21 +0100

    progress

--- EEPROM93LC66/testbench/EEPROM93LC66_tb.vhd
+++ EEPROM93LC66/testbench/EEPROM93LC66_tb.vhd
@@ -12,7 +12,7 @@ ARCHITECTURE verhalten OF EEPROM93LC66_tb IS
    CONSTANT FRQDEF: natural   := 1e6;
    CONSTANT tcyc:   time      := 1 sec / FRQDEF;
 
-   COMPONENT EEPROM93LC67
+   COMPONENT EEPROM93LC66
       PORT(sclk: IN  std_logic;  -- serial clock input
            cs:   IN  std_logic;  -- chip select, low active
            din:  IN  std_logic;  -- serial data input
@@ -27,52 +27,48 @@ ARCHITECTURE verhalten OF EEPROM93LC66_tb IS
    SIGNAL miso: std_logic := '0';
    SIGNAL mosi: std_logic := '0';
    SIGNAL org: std_logic := '0';
-   
-   SIGNAL reg:  std_logic_vector(15 DOWNTO 0);
 
 BEGIN
 
    --rst <= RSTDEF, NOT RSTDEF AFTER 5 us;
 --   clk <= NOT clk AFTER tcyc/2;
 
-   u1: EEPROM93LC67
+   u1: EEPROM93LC66
    PORT MAP(sclk => sclk,
             cs   => cs,
             din  => mosi,
             dout => miso,
 			org => org);
 	p1: PROCESS
-		PROCEDURE write (data: std_logic_vector; address: std_logic_vector) IS
-			CONSTANT oppcode: std_logic_vector(1 DOWNTO 0) := "01";
-			PROCEDURE spi_write (data: std_logic_vector) IS
-				VARIABLE cnt : integer := 0;
-			BEGIN
-				cs <= '1';
-				WAIT FOR tcyc;
-				sclk <= '1';
-				WAIT FOR tcyc;
-				sclk <= '0';
-				WAIT FOR tcyc;
-				mosi <= '1';
-				sclk <= '1';
-				WAIT FOR tcyc;
-				sclk <= '0';
-				WAIT FOR tcyc;
-				
-				FOR cnt IN 0 to 18 LOOP
-					mosi <= data(cnt);
-					sclk <= '1';
-					WAIT FOR tcyc;
-					sclk <= '0';
-					WAIT FOR tcyc;
-				END LOOP;
-
-				cs <= '0';
-				WAIT FOR 2*tcyc;
-			END PROCEDURE;
+		PROCEDURE spi_write (opcode: std_logic_vector; address: std_logic_vector;
+							 data: std_logic_vector; clkcycl: integer) IS
+			VARIABLE cnt : integer := 0;
+			VARIABLE serialOut: std_logic_vector(clkcycl - 2 DOWNTO 0);
 		BEGIN
-			--WAIT UNTIL clk'EVENT AND clk='1';
-			spi_write(oppcode & address & data);
+			serialOut := (opcode & address & data);
+
+			cs <= '1';
+			WAIT FOR tcyc;
+			sclk <= '1';
+			WAIT FOR tcyc;
+			sclk <= '0';
+			WAIT FOR tcyc;
+			mosi <= '1';
+			sclk <= '1';
+			WAIT FOR tcyc;
+			sclk <= '0';
+			WAIT FOR tcyc;
+			
+			FOR cnt IN clkcycl - 2 DOWNTO 0 LOOP
+				mosi <= serialOut(cnt);
+				sclk <= '1';
+				WAIT FOR tcyc;
+				sclk <= '0';
+				WAIT FOR tcyc;
+			END LOOP;
+
+			cs <= '0';
+			WAIT FOR 2*tcyc;
 		END PROCEDURE;
 		
 		--PROCEDURE delete (address: std_logic_vector) IS
@@ -102,7 +98,8 @@ BEGIN
 		--	END LOOP;
 		--END PROCEDURE;
 	BEGIN
-		write("10101010", "000000000");
+		-- write 8 bit
+		spi_write("01", "000000000", "10101010", 20);
 	END PROCESS;
 
 END verhalten;
