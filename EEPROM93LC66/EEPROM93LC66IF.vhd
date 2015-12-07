e9616b8eced1d632b540f5a580f750657da306ec
Matthias Weis <matweis@htwg-konstanz.de>
2015-12-07 17:57:23 +0100

    IF compiled

--- EEPROM93LC66/EEPROM93LC66IF.vhd
+++ EEPROM93LC66/EEPROM93LC66IF.vhd
@@ -11,8 +11,8 @@ ENTITY EEPROM93LC66IF IS
 			strb:	IN	std_logic;			-- executes the given command with the given address 
 			dout:	OUT std_logic_vector(15 DOWNTO 0);
 			din:	IN 	std_logic_vector(15 DOWNTO 0);
-			adrin	IN 	std_logic_vector(8 DOWNTO 0)
-			busy:	OUT	std_logic;			-- busy bit indicates working eeprom, dout not valid
+			adrin:	IN 	std_logic_vector(8 DOWNTO 0);
+			busyout:OUT	std_logic;			-- busy bit indicates working eeprom, dout not valid
 
 			sclk:	OUT std_logic;			-- serial clock to EEPROM
 			cs:		OUT std_logic;			-- chip select, HIGH active
@@ -33,51 +33,64 @@ END EEPROM93LC66IF;
 
 ARCHITECTURE behaviour OF EEPROM93LC66IF IS
 
-   	TYPE tstate IS (IDLE, INIT, BUSY);
+   	TYPE tstate IS (IDLE, BUSY, WAITEEPROM);
 
    	SIGNAL state:		tstate;
 	SIGNAL serialOut: 	std_logic_vector(26 DOWNTO 0);
+	SIGNAL serialIn:	std_logic_vector(15 DOWNTO 0);
 	SIGNAL serClk:		std_logic;
+	SIGNAL busyS:		std_logic;
 
+	SIGNAL outCnt:		integer RANGE 0 TO 27;
+	SIGNAL MemAccess:	std_logic;
 
 BEGIN
 
 	main: PROCESS(rst, clk) IS
-		SIGNAL outCnt:		integer RANGE 0 TO 27;
+		
 
 	BEGIN
 		IF rst = RSTDEF THEN
 			dout <= (others => '0');
-			busy <= '0';
+			busyS <= '0';
 
 			sclk <= '0';
 			cs <= '0';
 			org <= '0';
 			serialOut <= (others => '0');
+			serialIn <= (others => '0');
 
 			state <= IDLE;
 			serClk <= '0';
+
+			outCnt <= 0;
+			MemAccess <= '0';
 		ELSIF rising_edge(clk) THEN
 			CASE state IS
 				WHEN IDLE =>
 					IF strb = '1' THEN
+						cs <= '1';
 						CASE cmd IS
 							--16bit
 							WHEN "1001" => --16bit write
 								serialOut <= "101" & adrin(7 DOWNTO 0) & din;
 								outCnt <= 27;
+								MemAccess <= '1';
 							WHEN "1010" => --16bit read
 								serialOut <= "110" & adrin(7 DOWNTO 0) & "0000000000000000";
 								outCnt <= 27;
 							WHEN "1011" => --16bit erase
 								serialOut <= "111" & adrin(7 DOWNTO 0) & "0000000000000000";
 								outCnt <= 11;
+								MemAccess <= '1';
 							WHEN "1100" => --16bit eral
 								serialOut <= "10010000000" & "0000000000000000";
 								outCnt <= 11;
+								MemAccess <= '1';
 							WHEN "1101" => --16bit wral
 								serialOut <= "10001000000" & din;
 								outCnt <= 27;
+								MemAccess <= '1';
 							WHEN "1110" => --16bit EWEN
 								serialOut <= "10011000000" & "0000000000000000";
 								outCnt <= 11;
@@ -88,38 +101,65 @@ BEGIN
 							WHEN "0001" => --8bit write
 								serialOut <= "101" & adrin(8 DOWNTO 0) & din(7 DOWNTO 0) & "0000000";
 								outCnt <= 20;
+								MemAccess <= '1';
 							WHEN "0010" => --8bit read
 								serialOut <= "110" & adrin(8 DOWNTO 0) & "000000000000000";
 								outCnt <= 20;
 							WHEN "0011" => --8bit erase
 								serialOut <= "111" & adrin(8 DOWNTO 0) & "000000000000000";
 								outCnt <= 12;
+								MemAccess <= '1';
 							WHEN "0100" => --8bit eral
 								serialOut <= "10010000000" & "0000000000000000";
 								outCnt <= 12;
+								MemAccess <= '1';
 							WHEN "0101" => --8bit wral
 								serialOut <= "10001000000" & din(7 DOWNTO 0) & "0000000";
 								outCnt <= 20;
+								MemAccess <= '1';
 							WHEN "0110" => --8bit EWEN
 								serialOut <= "10011000000" & "0000000000000000";
 								outCnt <= 12;
 							WHEN "0111" => --8bit EWDS
 								serialOut <= "10000000000" & "0000000000000000";
 								outCnt <= 12;
+							WHEN others =>
+
 						END CASE;
-						busy <= '1';
+						busyS<= '1';
 						org <= cmd(3);
+						state <= BUSY;
 					END IF;
 				WHEN BUSY =>
 					IF serClk = '0' THEN
-						outCnt = outCnt - 1;
+						outCnt <= outCnt - 1;
+						serialIn <= serialIn(serialIn'LEFT-1 DOWNTO serialIn'RIGHT) & miso;
 						serClk <= '1';
-						IF(outCnt = 0) THEN
-							state <= IDLE;
-						END IF;
 					ELSE
 						serialOut <= serialOut(serialOut'LEFT-1 DOWNTO serialOut'RIGHT) & '0';
+						IF(outCnt = 0) THEN
+							IF MemAccess = '1' THEN
+								state <= WAITEEPROM;
+								outCnt <= 2;
+							ELSE
+								busyS<= '0';
+								state <= IDLE;
+							END IF;
+							dout <= serialIn;
+							cs <= '0';
+						END IF;
 						serClk <= '0';
+					END IF;
+				WHEN WAITEEPROM =>
+					IF outCnt /= 0 THEN
+						outCnt <= outCnt -1;
+					ELSE
+						cs <= '1';
+						IF miso = '1' THEN
+							cs <= '0';
+							state <= IDLE;
+							busyS<= '0';
+						END IF;
 					END IF;
 			END CASE;
 		END IF;
@@ -128,6 +168,7 @@ BEGIN
 
    mosi <= serialOut(serialOut'LEFT);
    sclk <= serClk;
+   busyout <= busyS;
 
 
 END behaviour;
