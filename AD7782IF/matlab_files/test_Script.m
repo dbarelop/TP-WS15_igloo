7e98aaf4d443d1d3676f51574f6ff0f28aed54ab
Lukas <lustoppe@htwg-konstanz.de>
2015-12-09 16:53:01 +0100

    add some variables for beter use of the Matlab ADC

--- AD7782IF/matlab_files/test_Script.m
+++ AD7782IF/matlab_files/test_Script.m
@@ -1,10 +1,14 @@
+N = 24;
+a_in = 0.15;
+gain = 16;
+Vref = 2.5;
 
-code = adcCodeGeneration(24, 0.0, 16.0, 2.5);
+code = adcCodeGeneration(N, a_in, gain, Vref);
 code_output = floor(code);
-bin_oi = decimalToBinaryVector(code_output, 24);
+bin_oi = decimalToBinaryVector(code_output, N);
 hex_out = binaryVectorToHex(bin_oi);
 
 code_input = binaryVectorToDecimal(bin_oi);
-ain = adcAINGeneration(24, code_input, 16.0, 2.5);
+ain = adcAINGeneration(N, code_input, gain, Vref);
 
 %Funktioniert so mit jedem input wert!
