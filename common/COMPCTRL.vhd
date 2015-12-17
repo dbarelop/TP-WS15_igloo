31c1ee3837216c13ca7d36aa34f4a504e6c36902
unknown <edc12@EDC12-PC.in.fhkn.de>
2015-12-17 14:02:21 +0100

    draft working

--- common/COMPCTRL.vhd
+++ common/COMPCTRL.vhd
@@ -4,7 +4,7 @@ USE ieee.std_logic_unsigned.ALL;
 
 ENTITY COMPXCTRL IS
 	GENERIC(RSTDEF: std_logic := '1';
-			DEVICEID: std_logic_vector(3 DOWNTO 0) := "0100");
+			DEVICEID: std_logic_vector(3 DOWNTO 0) := "0000");
 	PORT(    rst:		IN	std_logic;
 			 clk:		IN	std_logic;
 			 
@@ -22,10 +22,12 @@ END COMPXCTRL;
 
 ARCHITECTURE behaviour OF COMPXCTRL IS
 
-	TYPE tstate IS (IDLE, READSENDOK, EXECMD, ENDCOM);
+	TYPE tstate IS (IDLE, READSENDOK, WAITSENDOK, DELAY, EXECMD, ENDCOM);
+    --TYPE tcmd IS (GETVERSION);
 	
 	SIGNAL state: tstate;
 	SIGNAL dataIN: std_logic_vector(7 DOWNTO 0);
+    SIGNAL sbusy: std_logic;
 
 BEGIN
 
@@ -33,7 +35,7 @@ BEGIN
 
 	BEGIN
 		IF rst = RSTDEF THEN
-			busy <= 'Z';
+			sbusy <= 'Z';
 			uartout <= (others => 'Z');
 			uartTx <= 'Z';
 			uartRd <= 'Z';
@@ -41,33 +43,43 @@ BEGIN
 			state <= IDLE;
 		ELSIF rising_edge(clk) THEN
 			IF state = IDLE AND uartRx = '1' THEN
-				IF uartin(7 DOWNTO 4) = DEVICEID AND busy /= '1' THEN
-					busy <= '1';
+				IF uartin(7 DOWNTO 4) = DEVICEID AND sbusy /= '1' THEN
+					sbusy <= '1';
 					uartRd <= '1';
 					dataIN <= uartin;
 					state <= READSENDOK;
 				END IF;
 			ELSIF state = READSENDOK THEN
-				uartout <= x"5A"; -- OK message
+				uartout <= x"AA"; -- OK message
 				uartTx <= '1';
-				uartRd <= 'Z';
-				state <= EXECMD;
-			ELSIF state = EXECMD THEN
-				uartTx <= 'Z';
+				uartRd <= '0';
+				state <= DELAY;
+            ELSIF state = WAITSENDOK THEN                
+                uartTx <= '0';
+                IF uartTxReady = '1' THEN
+                    state <= EXECMD;
+                END IF;
+            ELSIF state = DELAY THEN
+                state <= WAITSENDOK;
+			ELSIF state = EXECMD THEN				
 				-- BEGIN handle command
 				CASE dataIN(3 DOWNTO 0) IS
 					WHEN others =>
-						state <= ENDCOM;
+						uartout <= x"01";
+                        uartTx <= '1';
+                        state <= ENDCOM;
 				END CASE;
 				-- END handle command
 			ELSIF state = ENDCOM THEN
 				uartout <= (others => 'Z');
 				uartTx <= 'Z';
 				uartRd <= 'Z';
-				busy <= 'Z';
+				sbusy <= 'Z';
 				state <= IDLE;
 			END IF;
 		END IF;
 	END PROCESS;
+
+    busy <= sbusy;
 
 END behaviour;
