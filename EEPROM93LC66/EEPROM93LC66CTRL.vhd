d5a648450328bb952818e377cada00137cd00367
Matthias Weis <matweis@htwg-konstanz.de>
2016-01-03 22:04:08 +0100

    refactoring eeprom ctrl & tb

--- EEPROM93LC66/EEPROM93LC66CTRL.vhd
+++ EEPROM93LC66/EEPROM93LC66CTRL.vhd
@@ -5,23 +5,23 @@ USE ieee.std_logic_unsigned.ALL;
 ENTITY EEPROMCTRL IS
 	GENERIC(RSTDEF: std_logic := '1';
 			DEVICEID: std_logic_vector(3 DOWNTO 0) := "0001");
-	PORT(    rst:		IN	std_logic;
-			 clk:		IN	std_logic;
+	PORT(	rst:		IN		std_logic;
+			clk:		IN		std_logic;
 			 
-			 uartin:	IN 	std_logic_vector(7 DOWNTO 0);
-			 uartRx:	IN	std_logic;						-- indicates new byte is available
-			 uartRd:	INOUT std_logic; 						-- indicates value was read from controller
-			 uartout:   INOUT std_logic_vector(7 DOWNTO 0);
-			 uartTxReady: IN std_logic;						-- indicates new byte can be send
-			 uartTx:	INOUT std_logic;						-- starts transmission of new byte
+			uartin:		IN 		std_logic_vector(7 DOWNTO 0);
+			uartRx:		IN		std_logic;						-- indicates new byte is available
+			uartRd:		INOUT 	std_logic;				-- indicates value was read from controller
+			uartout: 	INOUT 	std_logic_vector(7 DOWNTO 0);
+			uartTxReady:IN 		std_logic;						-- indicates new byte can be send
+			uartTx:		INOUT 	std_logic;						-- starts transmission of new byte
 			 
-			 busy:		INOUT	std_logic;					-- busy bit indicates working component
+			busy:		INOUT	std_logic;					-- busy bit indicates working component
 			-- component pins
-			sclk:		OUT std_logic;
-			cs:			OUT std_logic;
-			mosi:		OUT std_logic;
-			miso:		IN std_logic;
-			org:		OUT std_logic
+			sclk:		OUT 	std_logic;
+			cs:			OUT 	std_logic;
+			mosi:		OUT 	std_logic;
+			miso:		IN 		std_logic;
+			org:		OUT 	std_logic
 	);
 
 END EEPROMCTRL;
@@ -29,7 +29,7 @@ END EEPROMCTRL;
 ARCHITECTURE behaviour OF EEPROMCTRL IS
 
 	COMPONENT EEPROM93LC66IF
-    GENERIC(RSTDEF: std_logic);
+	GENERIC(RSTDEF: std_logic);
 	PORT(	rst:	IN	std_logic;
 			clk:	IN	std_logic;			-- 4 MHz MAX!! leads to 2 MHz sclk
 			cmd:	IN 	std_logic_vector(3 DOWNTO 0);
@@ -45,52 +45,52 @@ ARCHITECTURE behaviour OF EEPROMCTRL IS
 			miso:	IN 	std_logic;
 			org:	OUT std_logic);			-- memory-config =1 16 bit / =0 8 bit wordlength
 
-   END COMPONENT;
+	END COMPONENT;
 
-   -- component signals
-   SIGNAL cmd: std_logic_vector(3 DOWNTO 0);
-   SIGNAL strb: std_logic;
-   SIGNAL dout: std_logic_vector(15 DOWNTO 0);
-   SIGNAL din: std_logic_vector(15 DOWNTO 0);
-   SIGNAL adrin: std_logic_vector(8 DOWNTO 0);
-   SIGNAL busyout: std_logic;
+	-- component signals
+	SIGNAL cmd: std_logic_vector(3 DOWNTO 0);
+	SIGNAL strb: std_logic;
+	SIGNAL dout: std_logic_vector(15 DOWNTO 0);
+	SIGNAL din: std_logic_vector(15 DOWNTO 0);
+	SIGNAL adrin: std_logic_vector(8 DOWNTO 0);
+	SIGNAL busyout: std_logic;
 
 
 	TYPE tstate IS (IDLE, READSENDOK, WAITSENDOK, DELAY, DEFCMD, EXECMD, ENDCOM);
 	TYPE tmaincmd IS (READ, WRITE, ERASE, EWEN);
-	
+
 	SIGNAL state: tstate;
 	SIGNAL maincmd: tmaincmd;
 	SIGNAL dataIN: std_logic_vector(7 DOWNTO 0);
 
-    TYPE tcmd IS (SENDCMD, RXARG1, DELAY, RXARG2, WAITANSWER, TXANSWER);
+	TYPE tcmd IS (SENDCMD, RXARG1, DELAY, RXARG2, WAITANSWER, TXANSWER);
 	SIGNAL readcmd: tcmd;
 
 BEGIN
 
 	u2: EEPROM93LC66IF
-   GENERIC MAP(RSTDEF => RSTDEF)
-   PORT MAP(rst => rst,
-   			clk => clk,
-   			cmd => cmd,
-   			strb=> strb,
-   			dout=> dout,
-   			din => din,
-   			adrin => adrin,
-   			busyout => busyout,
+	GENERIC MAP(RSTDEF => RSTDEF)
+	PORT MAP(rst => rst,
+			clk => clk,
+			cmd => cmd,
+			strb=> strb,
+			dout=> dout,
+			din => din,
+			adrin => adrin,
+			busyout => busyout,
 
-   			sclk => sclk,
-   			cs => cs,
-   			mosi => mosi,
-   			miso => miso,
-   			org => org
+			sclk => sclk,
+			cs => cs,
+			mosi => mosi,
+			miso => miso,
+			org => org
 
-   		);
+			);
 
 	main: PROCESS (clk, rst) IS
 
 		PROCEDURE re4d IS
-			
+
 		BEGIN
 			IF readcmd = SENDCMD THEN
 				cmd <= "0010";
@@ -101,8 +101,8 @@ BEGIN
 					adrin <= '0' & uartin;
 					uartRd <= '1';
 					strb <= '1';
-                	readcmd <= WAITANSWER;
-                END IF;
+					readcmd <= WAITANSWER;
+				END IF;
 			ELSIF readcmd = WAITANSWER THEN
 				strb <= '0';
 				uartRd <= '0';
@@ -161,16 +161,16 @@ BEGIN
 				IF uartRx = '1' THEN
 					adrin <= '0' & uartin;
 					uartRd <= '1';
-                	readcmd <= DELAY;
-                END IF;
-            ELSIF readcmd = DELAY THEN
-            	uartRd <= '0';
-            	IF uartRx = '0' THEN
-            		readcmd <= RXARG2;
-            	END IF;
-           	ELSIF readcmd = RXARG2 THEN
-           		-- rx data
-           		IF uartRx = '1' THEN
+					readcmd <= DELAY;
+				END IF;
+			ELSIF readcmd = DELAY THEN
+				uartRd <= '0';
+				IF uartRx = '0' THEN
+					readcmd <= RXARG2;
+				END IF;
+			ELSIF readcmd = RXARG2 THEN
+				-- rx data
+				IF uartRx = '1' THEN
 					din(7 DOWNTO 0) <= uartin;
 					strb <= '1';
 					uartRd <= '1';
@@ -192,7 +192,7 @@ BEGIN
 			uartout <= (others => 'Z');
 			uartTx <= 'Z';
 			uartRd <= 'Z';
-			
+
 			state <= EXECMD;
 			maincmd <= EWEN;
 
@@ -200,7 +200,7 @@ BEGIN
 			strb <= '0';
 			din <= (others => '0');
 			adrin <= (others => '0');
-            readcmd <= SENDCMD;
+			readcmd <= SENDCMD;
 
 		ELSIF rising_edge(clk) THEN
 			IF state = IDLE AND uartRx = '1' THEN
@@ -215,33 +215,33 @@ BEGIN
 				uartTx <= '1';
 				uartRd <= '0';
 				state <= DELAY;
-            ELSIF state = DELAY THEN
-                state <= WAITSENDOK;
-            ELSIF state = WAITSENDOK THEN                
-                uartTx <= '0';
-                IF uartTxReady = '1' THEN
-                    state <= DEFCMD;
-                END IF;
-            ELSIF state = DEFCMD THEN
-            	CASE dataIN(3 DOWNTO 0) IS
-            		WHEN "0000" => -- read
-            			maincmd <= READ;
-                    WHEN x"1" => --write
-                        maincmd <= WRITE;
-            		WHEN x"2" => -- erase
-            			maincmd <= ERASE;
-            		WHEN others =>
-                        state <= ENDCOM;
-            	END CASE;
-                state <= EXECMD;
-			ELSIF state = EXECMD THEN				
+			ELSIF state = DELAY THEN
+				state <= WAITSENDOK;
+			ELSIF state = WAITSENDOK THEN
+				uartTx <= '0';
+				IF uartTxReady = '1' THEN
+					state <= DEFCMD;
+				END IF;
+			ELSIF state = DEFCMD THEN
+				CASE dataIN(3 DOWNTO 0) IS
+					WHEN "0000" => -- read
+						maincmd <= READ;
+					WHEN x"1" => --write
+						maincmd <= WRITE;
+					WHEN x"2" => -- erase
+						maincmd <= ERASE;
+					WHEN others =>
+						state <= ENDCOM;
+					END CASE;
+				state <= EXECMD;
+			ELSIF state = EXECMD THEN
 				-- BEGIN handle command
 				IF maincmd = ERASE THEN
 					erasePro;
 				ELSIF maincmd = READ THEN
 					re4d;
-                ELSIF maincmd = WRITE THEN
-                    writePro;
+				ELSIF maincmd = WRITE THEN
+					writePro;
 				ELSIF maincmd = EWEN THEN
 					ewenPro;
 				END IF;
@@ -255,7 +255,5 @@ BEGIN
 			END IF;
 		END IF;
 	END PROCESS;
-
-    --busy <= sbusy;
 
 END behaviour;
--- EEPROM93LC66/testbench/EEPROM93LC66CTRL_tb.vhd
+++ EEPROM93LC66/testbench/EEPROM93LC66CTRL_tb.vhd
@@ -3,31 +3,31 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY EEPROM93LC66CTRL_tb IS
-   -- empty
+	-- empty
 END EEPROM93LC66CTRL_tb;
 
 ARCHITECTURE verhalten OF EEPROM93LC66CTRL_tb IS
 
-   CONSTANT RSTDEF: std_logic := '0';
-   CONSTANT FRQDEF: natural   := 4e6;
-   CONSTANT tcyc:   time      := 1 sec / FRQDEF;
+	CONSTANT RSTDEF: 	std_logic 	:= '0';
+	CONSTANT FRQDEF: 	natural		:= 4e6;
+	CONSTANT tcyc:		time		:= 1 sec / FRQDEF;
 
-   CONSTANT cmd_wr: std_logic_vector := "001";
+	CONSTANT cmd_wr: std_logic_vector := "001";
 
-   COMPONENT EEPROMCTRL
-    GENERIC(RSTDEF: std_logic ;
+	COMPONENT EEPROMCTRL
+	GENERIC(RSTDEF: std_logic ;
 			DEVICEID: std_logic_vector);
-	PORT(    rst:		IN	std_logic;
-			 clk:		IN	std_logic;
+	PORT(	rst:		IN	std_logic;
+			clk:		IN	std_logic;
+
+			uartin:		IN 	std_logic_vector(7 DOWNTO 0);
+			uartRx:		IN	std_logic;						-- indicates new byte is available
+			uartRd:		INOUT std_logic; 						-- indicates value was read from controller
+			uartout:	INOUT std_logic_vector(7 DOWNTO 0);
+			uartTxReady:IN std_logic;						-- indicates new byte can be send
+			uartTx:		INOUT std_logic;						-- starts transmission of new byte
 			 
-			 uartin:	IN 	std_logic_vector(7 DOWNTO 0);
-			 uartRx:	IN	std_logic;						-- indicates new byte is available
-			 uartRd:	INOUT std_logic; 						-- indicates value was read from controller
-			 uartout:   INOUT std_logic_vector(7 DOWNTO 0);
-			 uartTxReady: IN std_logic;						-- indicates new byte can be send
-			 uartTx:	INOUT std_logic;						-- starts transmission of new byte
-			 
-			 busy:		INOUT	std_logic;					-- busy bit indicates working component
+			busy:		INOUT	std_logic;					-- busy bit indicates working component
 			-- component pins
 			sclk:		OUT std_logic;
 			cs:			OUT std_logic;
@@ -35,59 +35,59 @@ ARCHITECTURE verhalten OF EEPROM93LC66CTRL_tb IS
 			miso:		IN std_logic;
 			org:		OUT std_logic
 	);
-   END COMPONENT;
+	END COMPONENT;
 
-   COMPONENT EEPROM93LC66
-   PORT(sclk:		IN std_logic;
-			 cs:			IN std_logic;
-			 din:			IN std_logic;
-			 dout:			OUT std_logic;
-			 org:			IN std_logic);
-   END COMPONENT;
+	COMPONENT EEPROM93LC66
+	PORT(	sclk:	IN std_logic;
+			cs:		IN std_logic;
+			din:	IN std_logic;
+			dout:	OUT std_logic;
+			org:	IN std_logic);
+	END COMPONENT;
 
 
 
-   SIGNAL rst:  std_logic := RSTDEF;
-   SIGNAL clk:  std_logic := '0';
-   SIGNAL sclk: std_logic := '0';
-   SIGNAL cs:	std_logic := '0';
-   SIGNAL mosi: std_logic := '0';
-   SIGNAL miso: std_logic := '0';
-   SIGNAL org: 	std_logic := '0';
+	SIGNAL rst:	std_logic := RSTDEF;
+	SIGNAL clk:	std_logic := '0';
+	SIGNAL sclk:std_logic := '0';
+	SIGNAL cs:	std_logic := '0';
+	SIGNAL mosi:std_logic := '0';
+	SIGNAL miso:std_logic := '0';
+	SIGNAL org: std_logic := '0';
 
-   SIGNAL uartin:std_logic_vector(7 DOWNTO 0) := (others => '0');
-   SIGNAL uartRx:std_logic:= '0';
-   SIGNAL uartRd:std_logic:= '0';
-   SIGNAL uartout:std_logic_vector(7 DOWNTO 0) := (others => '0');
-   SIGNAL uartTxReady:std_logic :='0';
-   SIGNAL uartTx: std_logic:='0';
-   SIGNAL busy:	std_logic := '0';
+	SIGNAL uartin:std_logic_vector(7 DOWNTO 0) := (others => '0');
+	SIGNAL uartRx:std_logic:= '0';
+	SIGNAL uartRd:std_logic:= '0';
+	SIGNAL uartout:std_logic_vector(7 DOWNTO 0) := (others => '0');
+	SIGNAL uartTxReady:std_logic :='0';
+	SIGNAL uartTx: std_logic:='0';
+	SIGNAL busy:	std_logic := '0';
 
 BEGIN
 
 	clk <= NOT clk AFTER tcyc/2;
 
-   u1: EEPROM93LC66
-   PORT MAP(sclk => sclk,
-            cs   => cs,
-            din  => mosi,
-            dout => miso,
-	    org => org);
+	u1: EEPROM93LC66
+	PORT MAP(sclk 	=> sclk,
+			cs 		=> cs,
+			din 	=> mosi,
+			dout 	=> miso,
+			org 	=> org);
 
-   u2: EEPROMCTRL
-   GENERIC MAP(RSTDEF => RSTDEF,
-   				DEVICEID => "0001")
-   PORT MAP(rst		=>	rst,	
+	u2: EEPROMCTRL
+	GENERIC MAP(RSTDEF => RSTDEF,
+				DEVICEID => "0001")
+	PORT MAP(rst		=>	rst,
 			clk		=>	clk,
 			uartin	=>	uartin,
 			uartRx	=>	uartRx,
 			uartRd	=>	uartRd,
-			uartout	=>  uartout,
+			uartout	=>	uartout,
 			uartTxReady	=>	uartTxReady,
 			uartTx	=>	uartTx,
 			busy	=>	busy,
 			sclk	=>	sclk,
-			cs		=>	cs,	
+			cs		=>	cs,
 			mosi	=>	mosi,
 			miso	=>	miso,
 			org		=>	org
@@ -122,11 +122,11 @@ BEGIN
 			END IF;
 
 		END PROCEDURE;
-		
+
 	BEGIN
-   		WAIT FOR 1 us;
-   		rst <= NOT RSTDEF;
-   		
+		WAIT FOR 1 us;
+		rst <= NOT RSTDEF;
+
 		n_bytes:= 2;
 		uartSendN("00010000"&"00000000", x"FF");
 		n_bytes:= 3;
@@ -146,8 +146,8 @@ BEGIN
 		uartSendN("00010000"&"00000010", x"FF");
 
 
-      	REPORT "all tests done..." SEVERITY note;
-	WAIT;
+		REPORT "all tests done..." SEVERITY note;
+		WAIT;
 
 	END PROCESS;
 
