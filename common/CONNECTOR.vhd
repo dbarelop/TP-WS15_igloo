9346b8d43e3025c1e6b6e1872f8a94b811e62a02
Matthias Weis <matweis@htwg-konstanz.de>
2016-01-04 19:50:18 +0100

    connector complete

--- common/CONNECTOR.vhd
+++ common/CONNECTOR.vhd
@@ -7,7 +7,13 @@ ENTITY CONNECTOR IS
 			clk:		IN	std_logic;
 
 			uartRx:		IN	std_logic;
-			uartTx:		OUT std_logic
+			uartTx:		OUT std_logic;
+
+			eepromCS:	OUT std_logic;
+			eepromSCLK:	OUT std_logic;
+			eepromMOSI: OUT std_logic;
+			eepromMISO: IN  std_logic;
+			eepromORG:	OUT std_logic
 
 	);
 
@@ -26,29 +32,72 @@ ARCHITECTURE behaviour OF CONNECTOR IS
 	SIGNAL dout:	std_logic_vector(7 DOWNTO 0);
 
 	SIGNAL uartTxReady: std_logic;
+	SIGNAL busy:		std_logic;
 
 	COMPONENT uart 
-			GENERIC(RSTDEF: std_logic;
-					BAUDEF: real;  -- baud rate
-					FRQDEF: real);  -- clock frequency
-		 	PORT(rst:	IN 	std_logic;  -- reset RSTDEF active
-				clk:	IN 	std_logic;  -- clock, rising edge active
-				swrst:	IN 	std_logic;  -- software reset,  RSTDEF active
-				ena:	IN 	std_logic;  -- enable,		  high active
+		GENERIC(RSTDEF: std_logic;
+				BAUDEF: real;  -- baud rate
+				FRQDEF: real);  -- clock frequency
+	 	PORT(rst:	IN 	std_logic;  -- reset RSTDEF active
+			clk:	IN 	std_logic;  -- clock, rising edge active
+			swrst:	IN 	std_logic;  -- software reset,  RSTDEF active
+			ena:	IN 	std_logic;  -- enable,		  high active
 
-				rxd:	IN 	std_logic;  -- receive data
-				rden:	IN 	std_logic;  -- read enable,	 high active
-				dout:	OUT std_logic_vector(7 DOWNTO 0);
-				rhrf:	OUT std_logic;  -- RHR full,		high active
-				ovre:	OUT std_logic;  -- overrun error,   high active
-				frme:	OUT std_logic;  -- framing error,   high active
+			rxd:	IN 	std_logic;  -- receive data
+			rden:	IN 	std_logic;  -- read enable,	 high active
+			dout:	OUT std_logic_vector(7 DOWNTO 0);
+			rhrf:	OUT std_logic;  -- RHR full,		high active
+			ovre:	OUT std_logic;  -- overrun error,   high active
+			frme:	OUT std_logic;  -- framing error,   high active
 
-				txd:	OUT std_logic;  -- transmit data output, high active
-				wren:	IN 	std_logic;  -- write enable, high active
-				din:	IN 	std_logic_vector(7 DOWNTO 0); -- data input
-				tsre:	OUT std_logic;  -- transmit shift   register empty, high active
-				thre:	OUT std_logic); -- transmit holding register empty, high active
+			txd:	OUT std_logic;  -- transmit data output, high active
+			wren:	IN 	std_logic;  -- write enable, high active
+			din:	IN 	std_logic_vector(7 DOWNTO 0); -- data input
+			tsre:	OUT std_logic;  -- transmit shift   register empty, high active
+			thre:	OUT std_logic -- transmit holding register empty, high active
+			);
 	END COMPONENT;
+
+	COMPONENT COMPXCTRL
+		GENERIC(RSTDEF: 	std_logic;
+				DEVICEID: 	std_logic_vector(3 DOWNTO 0));
+		PORT(rst:		IN	std_logic;
+			clk:		IN	std_logic;
+
+			uartin:		IN 	std_logic_vector(7 DOWNTO 0);
+			uartRx:		IN	std_logic;						-- indicates new byte is available
+			uartRd:		INOUT std_logic; 						-- indicates value was read from controller
+			uartout:	INOUT std_logic_vector(7 DOWNTO 0);
+			uartTxReady:IN 	std_logic;						-- indicates new byte can be send
+			uartTx:		INOUT std_logic;						-- starts transmission of new byte
+
+			busy:		INOUT	std_logic					-- busy bit indicates working component
+			);
+	END COMPONENT;
+
+	COMPONENT EEPROMCTRL
+		GENERIC(RSTDEF: std_logic;
+				DEVICEID: std_logic_vector(3 DOWNTO 0));
+		PORT(rst:		IN		std_logic;
+			clk:		IN		std_logic;
+			 
+			uartin:		IN 		std_logic_vector(7 DOWNTO 0);
+			uartRx:		IN		std_logic;						-- indicates new byte is available
+			uartRd:		INOUT 	std_logic;				-- indicates value was read from controller
+			uartout: 	INOUT 	std_logic_vector(7 DOWNTO 0);
+			uartTxReady:IN 		std_logic;						-- indicates new byte can be send
+			uartTx:		INOUT 	std_logic;						-- starts transmission of new byte
+				 
+			busy:		INOUT	std_logic;					-- busy bit indicates working component
+			-- component pins
+			sclk:		OUT 	std_logic;
+			cs:			OUT 	std_logic;
+			mosi:		OUT 	std_logic;
+			miso:		IN 		std_logic;
+			org:		OUT 	std_logic
+		);
+	END COMPONENT;
+
 
 BEGIN
 
@@ -59,23 +108,61 @@ BEGIN
 				BAUDEF => 9.6e3,
 				FRQDEF => 1.0e6)
 	PORT MAP(
-			rst		=>	rst,  -- reset RSTDEF active
-			clk		=>	clk,  -- clock, rising edge active
-			swrst	=>	'0',  -- software reset,  RSTDEF active
-			ena		=>	'1',  -- enable,		  high active
+			rst		=>	rst,
+			clk		=>	clk,
+			swrst	=>	NOT RSTDEF,
+			ena		=>	'1',
 
-			rxd		=>	uartRx,  -- receive data
-			rden	=>	rden,  -- read enable,	 high active
+			rxd		=>	uartRx,
+			rden	=>	rden,
 			dout	=>	dout,
-			rhrf	=>	rhrf,  -- RHR full,		high active
-			ovre	=>	OPEN,  -- overrun error,   high active
-			frme	=>	OPEN,  -- framing error,   high active
+			rhrf	=>	rhrf,
+			ovre	=>	OPEN,
+			frme	=>	OPEN,
 
-			txd		=>	uartTx,  -- transmit data output, high active
-			wren	=>	wren,  -- write enable, high active
-			din		=>	din, -- data input
-			tsre	=>	tsre,  -- transmit shift   register empty, high active
-			thre	=>	thre -- transmit holding register empty, high active
+			txd		=>	uartTx,
+			wren	=>	wren,
+			din		=>	din, 
+			tsre	=>	tsre,
+			thre	=>	thre 
 		);
+
+	m1: COMPXCTRL
+	GENERIC MAP(RSTDEF	=> 	RSTDEF,
+				DEVICEID=> 	"0000")
+	PORT MAP(rst	=>		rst,
+			clk		=>		clk,
+
+			uartin	=>		dout,
+			uartRx	=>		rhrf,
+			uartRd	=>		rden,
+			uartout	=>		din,
+			uartTxReady	=>	uartTxReady,
+			uartTx	=>		wren,
+
+			busy	=>		busy
+			);
+
+	d1: EEPROMCTRL
+	GENERIC MAP(RSTDEF	=>	RSTDEF,
+			DEVICEID	=>	"0001")
+	PORT MAP(rst	=>		rst,
+			clk		=>		clk,
+			 
+			uartin	=>		dout,
+			uartRx	=>		rhrf,				-- indicates new byte is available
+			uartRd	=>		rden,				-- indicates value was read from controller
+			uartout	=> 		din,
+			uartTxReady	=>	uartTxReady,		-- indicates new byte can be send
+			uartTx	=>		wren,				-- starts transmission of new byte
+			 
+			busy	=>		busy,				-- busy bit indicates working component
+			-- component pins
+			sclk	=>		eepromSCLK,
+			cs		=>		eepromCS,
+			mosi	=>		eepromMOSI,
+			miso	=>		eepromMISO,
+			org		=>		eepromORG
+			);
 
 END behaviour;
