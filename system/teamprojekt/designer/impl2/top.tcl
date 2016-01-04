05dd07143f6db94efb34c9b67dfb1a9e9ef674b6
Matthias Weis <matweis@htwg-konstanz.de>
2016-01-05 00:53:34 +0100

    new project with connector

--- system/teamprojekt/designer/impl2/top.tcl
+++ system/teamprojekt/designer/impl2/top.tcl
@@ -1,12 +1,13 @@
 # Created by Microsemi Libero Software 11.6.0.34
-# Tue Dec 22 21:49:23 2015
+# Mon Jan 04 19:58:50 2016
 
 # (OPEN DESIGN)
 
-open_design "top.adb"
 
 # set default back-annotation base-name
 set_defvar "BA_NAME" "top_ba"
+set_defvar "KEEP_EXISTING_PHYSICAL_CONSTRAINTS" "1"
+set_defvar "SDC_IMPORT_MERGE" "1"
 set_defvar "IDE_DESIGNERVIEW_NAME" {Impl2}
 set_defvar "IDE_DESIGNERVIEW_COUNT" "2"
 set_defvar "IDE_DESIGNERVIEW_REV0" {Impl1}
@@ -15,10 +16,3 @@ set_defvar "IDE_DESIGNERVIEW_REV1" {Impl2}
 set_defvar "IDE_DESIGNERVIEW_REVNUM1" "2"
 set_defvar "IDE_DESIGNERVIEW_ROOTDIR" {C:\Users\matze\Documents\igloo\system\teamprojekt\designer}
 set_defvar "IDE_DESIGNERVIEW_LASTREV" "2"
-
-report -type "timing" -format "TEXT" -analysis "max" -print_summary "yes" -use_slack_threshold "no"                             -print_paths "yes" -max_paths 5 -max_expanded_paths 1                             -max_parallel_paths 1 -include_user_sets "no"                             -include_pin_to_pin "yes" -include_clock_domains "yes"                             -select_clock_domains "no" {top_maxdelay_timing_report.txt}
-report -type "timing" -format "TEXT" -analysis "min" -print_summary "yes" -use_slack_threshold "no"                             -print_paths "yes" -max_paths 5 -max_expanded_paths 1                             -max_parallel_paths 1 -include_user_sets "no"                             -include_pin_to_pin "yes" -include_clock_domains "yes"                             -select_clock_domains "no" {top_mindelay_timing_report.txt}
-report -type "timing_violations" -format "TEXT" -analysis "max" -use_slack_threshold "yes" -slack_threshold 0.00                               -limit_max_paths "yes" -max_paths 100 -max_expanded_paths 0                               -max_parallel_paths 1 {top_maxdelay_timingviolations_report.txt}
-report -type "timing_violations" -format "TEXT" -analysis "min" -use_slack_threshold "yes" -slack_threshold 0.00                               -limit_max_paths "yes" -max_paths 100 -max_expanded_paths 0                               -max_parallel_paths 1 {top_mindelay_timingviolations_report.txt}
-
-save_design
