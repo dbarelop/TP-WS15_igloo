0cbaf452c9c56c58210c4510ba992e70cf600204
Lukas Stoppel <lustoppe@htwg-konstanz.de>
2016-01-07 11:39:00 +0100

    Create Controller File, at Team: Könnt jetzt heim gehn ;)

---
+++ AD7782IF/AD7782CTRL.vhd
@@ -0,0 +1,12 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+ENTITY AD7782CTRL IS
+-- now empty
+END AD7782CTRL;
+
+
+ARCHITECTURE behaviour OF AD7782CTRL IS
+
+END behaviour;
